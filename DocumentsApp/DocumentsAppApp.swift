4f7cc053a674bf85ff3f8d124c0a49ed675eb006
Yurii Voievodin <voevodin.yura@icloud.com>
2025-06-08 09:46:28 +0200

    Blur UI in app switcher

--- DocumentsApp/DocumentsAppApp.swift
+++ DocumentsApp/DocumentsAppApp.swift
@@ -12,6 +12,9 @@ import SwiftData
 struct DocumentsAppApp: App {
     let container: ModelContainer
     
+    @Environment(\.scenePhase) private var scenePhase
+    @State private var blurRadius: CGFloat = 0
+    
     init() {
         do {
             // Configure SwiftData container
@@ -26,7 +29,15 @@ struct DocumentsAppApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView(modelContext: container.mainContext)
+                .blur(radius: blurRadius)
         }
         .modelContainer(container)
+        .onChange(of: scenePhase) { oldValue, newValue in
+            if newValue == .inactive || newValue == .background {
+                withAnimation { blurRadius = 20 }
+            } else {
+                withAnimation { blurRadius = 0 }
+            }
+        }
     }
 }
